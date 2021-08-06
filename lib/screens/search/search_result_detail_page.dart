b62ce6f0babca171b943e67704fd9bf84b0ed99d
Nick Gutierrez <cimbyote@gmail.com>
2021-08-06 18:09:19 -0500

    fleshed out detailed screen some more

--- lib/screens/search/search_result_detail_page.dart
+++ lib/screens/search/search_result_detail_page.dart
@@ -14,7 +14,6 @@ import 'package:monsoomer/services/api_service.dart';
 
 //TODO: search different API for more detailed info
 class SearchResultDetailPage extends StatefulWidget {
-
   late bool loadFlag = false;
 
   final MediaFromAPI tappedMediaItem;
@@ -35,23 +34,19 @@ class _SearchResultDetailPageState extends State<SearchResultDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    if(widget.loadFlag == false)
-      {
-        _apiService.searchAPIForDetailedInfo(widget.tappedMediaItem.id).then((value)
-        {
-          detailedTappedMediaItem = value;
-          setState(() {
-            loading = false;
-            widget.loadFlag = true;
-          });
+    if (widget.loadFlag == false) {
+      _apiService
+          .searchAPIForDetailedInfo(widget.tappedMediaItem.id)
+          .then((value) {
+        detailedTappedMediaItem = value;
+        setState(() {
+          loading = false;
+          widget.loadFlag = true;
         });
-      }
-    else
-      {
-        loading = false;
-      }
-
+      });
+    } else {
+      loading = false;
+    }
 
     String displayImage;
 
@@ -61,139 +56,182 @@ class _SearchResultDetailPageState extends State<SearchResultDetailPage> {
       //TODO: find better placeholder
       displayImage = 'https://i.stack.imgur.com/y9DpT.jpg';
     }
-    return loading ? LoadingWidget() : 
-    
-    Container(
-      height: MediaQuery.of(context).size.height - 160,
-      child:ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CachedNetworkImage(
-              height: 250,
-              imageUrl: displayImage,
-              placeholder: (context, url) => LoadingWidget(),
+    return loading
+        ? LoadingWidget()
+        : Container(
+            height: MediaQuery.of(context).size.height - 160,
+            child: ListView(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Card(
+                        child: CachedNetworkImage(
+                          height: 250,
+                          imageUrl: displayImage,
+                          placeholder: (context, url) => LoadingWidget(),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: AutoSizeText(
+                                detailedTappedMediaItem.title ?? 'NULL',
+                                maxLines: 2,
+                                maxFontSize: 32.0,
+                                style: TextStyle(fontSize: 32),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                detailedTappedMediaItem.year ?? 'NULL',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                detailedTappedMediaItem.type ?? 'NULL',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                detailedTappedMediaItem.actors ?? 'NULL',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                detailedTappedMediaItem.runtime ?? 'NULL',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      Text('Plot: ' + (detailedTappedMediaItem.plot ?? '')),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      Text('Genre: ' + (detailedTappedMediaItem.genre ?? '')),
+                      Text('Rated: ' + (detailedTappedMediaItem.rated ?? '')),
+                      Text('Released: ' +
+                          (detailedTappedMediaItem.released ?? '')),
+                      Text('Rated: ' + (detailedTappedMediaItem.rated ?? '')),
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Which List would you like to add this to?',
+                        style: TextStyle(fontSize: 24),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 10, 8, 0),
+                      child: ToggleSwitch(
+                        minWidth: 180.0,
+                        minHeight: 50.0,
+                        initialLabelIndex: showConsumedToggle,
+                        cornerRadius: 20.0,
+                        activeFgColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveFgColor: Colors.white,
+                        totalSwitches: 3,
+                        labels: [
+                          'In Progress List',
+                          'Wanted List',
+                          'Consumed List',
+                        ],
+                        iconSize: 30.0,
+                        borderColor: [
+                          Color(0xff3b5998),
+                          Color(0xff8b9dc3),
+                          Color(0xff00aeff),
+                          Color(0xff0077f2),
+                          Color(0xff962fbf),
+                          Color(0xff4f5bd5)
+                        ],
+                        dividerColor: Colors.blueGrey,
+                        activeBgColors: [
+                          [Color(0xff3b5998), Color(0xff8b9dc3)],
+                          [Color(0xff00aeff), Color(0xff0077f2)],
+                          [
+                            Color(0xfffeda75),
+                            Color(0xfffa7e1e),
+                            Color(0xffd62976),
+                            Color(0xff962fbf),
+                            Color(0xff4f5bd5)
+                          ]
+                        ],
+                        onToggle: (index) {
+                          setState(() {
+                            showConsumedToggle = index;
+                          });
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RoundedSquareButton(
+                        buttonText: 'Add To List',
+                        onPressedCallback: () {
+                          DatabaseService(
+                                  uid: Provider.of<UserInfoModel?>(context,
+                                          listen: false)!
+                                      .uid)
+                              .addToList(Media.fromAPI(
+                                  mediaFromAPI: widget.tappedMediaItem,
+                                  inStatus:
+                                      getStatusFromToggle(showConsumedToggle)));
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AutoSizeText(
-                  widget.tappedMediaItem.title,
-                  maxLines: 1,
-                  maxFontSize: 32.0,
-                  style: TextStyle(fontSize: 32),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.tappedMediaItem.year,
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.tappedMediaItem.type,
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  detailedTappedMediaItem.actors ?? 'NULL',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Which List would you like to add this to?',
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 10, 8, 0),
-                child: ToggleSwitch(
-                  minWidth: 180.0,
-                  minHeight: 50.0,
-                  initialLabelIndex: showConsumedToggle,
-                  cornerRadius: 20.0,
-                  activeFgColor: Colors.white,
-                  inactiveBgColor: Colors.grey,
-                  inactiveFgColor: Colors.white,
-                  totalSwitches: 3,
-                  labels: [
-                    'In Progress List',
-                    'Wanted List',
-                    'Consumed List',
-                  ],
-                  iconSize: 30.0,
-                  borderColor: [
-                    Color(0xff3b5998),
-                    Color(0xff8b9dc3),
-                    Color(0xff00aeff),
-                    Color(0xff0077f2),
-                    Color(0xff962fbf),
-                    Color(0xff4f5bd5)
-                  ],
-                  dividerColor: Colors.blueGrey,
-                  activeBgColors: [
-                    [Color(0xff3b5998), Color(0xff8b9dc3)],
-                    [Color(0xff00aeff), Color(0xff0077f2)],
-                    [
-                      Color(0xfffeda75),
-                      Color(0xfffa7e1e),
-                      Color(0xffd62976),
-                      Color(0xff962fbf),
-                      Color(0xff4f5bd5)
-                    ]
-                  ],
-                  onToggle: (index) {
-                    setState(() {
-                      showConsumedToggle = index;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RoundedSquareButton(
-                  buttonText: 'Add To List',
-                  onPressedCallback: () {
-                    DatabaseService(uid: Provider.of<UserInfoModel?>(context, listen: false)!.uid).addToList(Media.fromAPI(mediaFromAPI: widget.tappedMediaItem, inStatus: getStatusFromToggle(showConsumedToggle)));
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                    },
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+          );
   }
-  
-  MediaStatus getStatusFromToggle(int toggle)
-  {
+
+  MediaStatus getStatusFromToggle(int toggle) {
     if (toggle == 0) {
       return MediaStatus.Started;
     } else if (toggle == 1) {
       return MediaStatus.Wanted;
     } else if (toggle == 2) {
       return MediaStatus.Consumed;
-    }
-    else
-    {
+    } else {
       return MediaStatus.NULL;
     }
   }
